308d3b72fb0675982cc246959cc80432bb3810fb
Romy Rose Jimmy <romybijoy@gmail.com>
2026-04-10 17:39:21 +0530

    fix pom and switch to jar for railway

--- src/main/java/com/project/raiserbuddy/RaiserbuddyApplication.java
+++ src/main/java/com/project/raiserbuddy/RaiserbuddyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "com.project.raiserbuddy")
 @SpringBootApplication
 public class RaiserbuddyApplication extends SpringBootServletInitializer{
 
